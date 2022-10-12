c03a76256ebe52863d56230a3bc83c96b2c9c1a9
amor9121 <amor9121@gmail.com>
2022-10-12 16:24:21 +0800

    add fName/lName

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,73 @@ import React, { useState } from "react";
 function App() {
   const [headingText, setHeadingText] = useState("Hello");
   const [isMouseOver, setMouseOver] = useState(false);
-  const [name, setName] = useState("");
+  // const [name, setName] = useState("");
+  // const [fName, setfName] = useState("");
+  // const [lName, setlName] = useState("");
+  const [fullName, setFullName] = useState({
+    fName:"",
+    lName:""
+  });
+
 
   function handleClick(event) {
-    setHeadingText("Hello, " + name);
+    // setHeadingText("Hello, " + name);
+    setHeadingText("Hello, " + fullName.fName + " "+ fullName.lName);
     event.preventDefault(); //for Form submitted
-  }
+  }  
+
+  // function handleChange(event) {
+  //   setName(event.target.value);
+  // }
+
+  // function handleFNameChange(event) {
+  //   setfName(event.target.value);
+  // }
+
+  // function handleLNameChange(event) {
+  //   setlName(event.target.value);
+  // }
 
   function handleChange(event) {
-    setName(event.target.value);
-  }
+    // const newValue= event.target.value;
+    // const inputName = event.target.name;
 
+    const {value, name} = event.target;
+
+    // if(inputName === "fName"){
+    //   setFullName({fName:newValue});
+    // }else if (inputName === "lName"){
+    //   setFullName({lName:newValue});
+    // }
+
+    // setFullName(preValue => {
+    //   if(inputName === "fName"){
+    //       return{
+    //         fName:newValue,
+    //         lName:preValue.lName
+    //       };
+    //     }else if (inputName === "lName"){
+    //       return{
+    //         fName:preValue.fName,
+    //         lName:newValue
+    //       };
+    //   }
+    // })
+    setFullName(preValue => {
+      if(name === "fName"){
+          return{
+            fName:value,
+            lName:preValue.lName
+          };
+        }else if (name === "lName"){
+          return{
+            fName:preValue.fName,
+            lName:value
+          };
+      }
+    })
+  }
+ 
   function handleMouseOver() {
     setMouseOver(true);
   }
@@ -26,11 +82,31 @@ function App() {
     <div className="container">
       <h1>{headingText}</h1>
       <form onSubmit={handleClick}>
+        {/* <input
+          onChange={handleFNameChange}
+          type="text"
+          placeholder="What's your first name?"
+          value={fName}
+        />
         <input
+          onChange={handleLNameChange}
+          type="text"
+          placeholder="What's your last name?"
+          value={lName}
+        /> */}
+        <input
+          name="fName"
           onChange={handleChange}
           type="text"
-          placeholder="What's your name?"
-          value={name}
+          placeholder="What's your first name?"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your last name?"
+          value={fullName.lName}
         />
         <button
           style={{ backgroundColor: isMouseOver ? "black" : "white" }}
