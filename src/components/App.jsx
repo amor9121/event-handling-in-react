59e9fc0735092c6ad33e9b6d0df0947d25cf08fe
amor9121 <amor9121@gmail.com>
2022-10-12 12:20:19 +0800

    Init

---
+++ src/components/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+  const [name, setName] = useState("");
+
+  function handleClick(event) {
+    setHeadingText("Hello, " + name);
+    event.preventDefault(); //for Form submitted
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onClick={handleClick}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
